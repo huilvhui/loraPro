675a6c4b7a0e7b856ab6fce1c9c4646dd95dbd37
myCodeSpace <get110000@163.com>
2019-07-24 11:38:00 +0800

    Create IMessageSegment.java

---
+++ loraProtocol/src/main/java/com/xier/lorawan/model/IMessageSegment.java
@@ -0,0 +1,13 @@
+package com.xier.lorawan.model;
+
+/**
+ * function:
+ * date:2017-09-12 14:29
+ * author: he dong yu
+ * email:554151811@qq.com
+ */
+public interface IMessageSegment {
+
+    int length();
+
+}
