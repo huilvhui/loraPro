9a6fd7b7f1e853e325e7778775f519c68c12984b
myCodeSpace <get110000@163.com>
2019-07-24 11:03:18 +0800

    Create UpDownLink.java

---
+++ loraProtocol/src/main/java/com/xier/lorawan/enums/UpDownLink.java
@@ -0,0 +1,12 @@
+package com.xier.lorawan.enums;
+
+/**
+ * function:
+ * date:2017-09-09 14:36
+ */
+public enum UpDownLink {
+    //上行 来自设备--> server
+    UP_LINK,
+    //下行 来自server--> 设备down
+    DOWN_LINK
+}
