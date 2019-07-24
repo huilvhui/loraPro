88001c0ddbd460799e8f7e57ee82e3a810fdfdb6
myCodeSpace <get110000@163.com>
2019-07-24 11:35:27 +0800

    Create MockMacCommandAntHandlerImpl.java

---
+++ loraProtocol/src/main/java/com/xier/lorawan/mac/MockMacCommandAntHandlerImpl.java
@@ -0,0 +1,17 @@
+package com.xier.lorawan.mac;
+
+
+public class MockMacCommandAntHandlerImpl implements MacCommandAntHandler{
+
+	@Override
+    public void linkCheck() {    
+    }
+
+	@Override
+    public void devStatus(int battery) { 
+    }
+
+	@Override
+    public void linkAdr(String result) { 
+    }
+}
