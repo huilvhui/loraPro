daca0f54d65e67796052449c7b51f2945bbc6d60
myCodeSpace <get110000@163.com>
2019-07-24 11:32:59 +0800

    Create MacCommandHandlerAdapter.java

---
+++ loraProtocol/src/main/java/com/xier/lorawan/mac/MacCommandHandlerAdapter.java
@@ -0,0 +1,36 @@
+package com.xier.lorawan.mac;
+
+/**
+ * mac命令处理适配器
+ * <p></p>
+ * @author lvhui5 2018年2月1日 下午4:10:43
+ * @version V1.0
+ */
+public class MacCommandHandlerAdapter implements MacCommandAntHandler{
+
+	
+	public void antHandler(){
+		
+		
+		
+	}
+	
+	@Override
+    public void linkCheck() {
+	    // TODO Auto-generated method stub
+	    
+    }
+
+	@Override
+    public void devStatus(int battery) {
+	    // TODO Auto-generated method stub
+	    
+    }
+
+	@Override
+    public void linkAdr(String result) {
+	    // TODO Auto-generated method stub
+	    
+    }
+	
+}
