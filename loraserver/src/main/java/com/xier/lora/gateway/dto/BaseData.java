971557939b84e27b854faa8f21cf19bf95d7e0b2
myCodeSpace <get110000@163.com>
2019-07-24 17:35:50 +0800

    Create BaseData.java

---
+++ loraserver/src/main/java/com/xier/lora/gateway/dto/BaseData.java
@@ -0,0 +1,14 @@
+package com.xier.lora.gateway.dto;
+
+/**
+ * 基础数据
+ * <p>
+ * 1、上行json数据
+ * 2、下行json请求与返回结果
+ * </p>
+ * @author lvhui5 2017年11月20日 下午6:49:29
+ * @version V1.0
+ */
+public class BaseData {
+
+}
