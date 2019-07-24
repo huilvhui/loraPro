90ef6fc138418de35551a7fbf7675c29c168742e
myCodeSpace <get110000@163.com>
2019-07-24 15:59:35 +0800

    Create Message.java

---
+++ loraProtocol/src/main/java/com/xier/lorawan/model/message/Message.java
@@ -0,0 +1,10 @@
+package com.xier.lorawan.model.message;
+
+import com.xier.lorawan.model.IMessageSegment;
+
+/**
+ * function:
+ * date:2017-09-07 8:31
+ */
+public abstract class Message implements IMessageSegment{
+}
